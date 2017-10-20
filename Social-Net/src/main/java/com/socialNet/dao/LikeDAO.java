ce053a0093a8f69092582c33985480933c4deb07
Valentin Velinov <valentin.velinov@abv.bg>
2017-10-20 17:51:39 +0300

    Fixed Like and created test for it.

--- Social-Net/src/main/java/com/socialNet/dao/LikeDAO.java
+++ Social-Net/src/main/java/com/socialNet/dao/LikeDAO.java
@@ -8,18 +8,22 @@ import java.sql.Statement;
 
 import com.socialNet.dbmanager.DBConnection;
 import com.socialNet.exceptions.LikeException;
+import com.socialNet.exceptions.PostException;
+import com.socialNet.exceptions.UserException;
+import com.socialNet.pojo.Like;
 import com.socialNet.pojo.Post;
 import com.socialNet.pojo.User;
 
 public class LikeDAO {
-	private static final String INSERT_LIKE_SQL = "INSERT INTO likes VALUES (?,?)";
+	private static final String INSERT_LIKE_SQL = "INSERT INTO likes VALUES (null,?,?)";
+	private static final String SELECT_LIKE_BY_ID = "SELECT * FROM likes WHERE like_id=?";
 	
-	public int likePost (User user,Post post) throws LikeException {
+	public int likePost (Like like) throws LikeException {
 		Connection con = DBConnection.getInstance().getConnection();
 		try {
 			PreparedStatement ps = con.prepareStatement(INSERT_LIKE_SQL, Statement.RETURN_GENERATED_KEYS);
-			ps.setInt(1, post.getPost_id());
-			ps.setInt(2, user.getUser_id());
+			ps.setInt(1, like.getPost_id());
+			ps.setInt(2, like.getUser_id());
 			ps.executeUpdate();
 			ResultSet rs = ps.getGeneratedKeys();
 			rs.next();
--- Social-Net/src/main/java/com/socialNet/pojo/Like.java
+++ Social-Net/src/main/java/com/socialNet/pojo/Like.java
@@ -1,22 +1,46 @@
 package com.socialNet.pojo;
 
+import com.socialNet.exceptions.LikeException;
+
 public class Like {
+	private int like_id;
 	private int post_id;
 	private int user_id;
+	
+	public Like(int like_id, int post_id, int user_id) throws LikeException {
+		super();
+		setLike_id(like_id);
+		setPost_id(post_id);
+		setUser_id(user_id);
+	}
 
-	public Like(int post_id, int user_id) {
+	public Like(int post_id, int user_id) throws LikeException {
 		super();
 		setPost_id(post_id);
 		setUser_id(user_id);
+	}
+
+	public int getLike_id() {
+		return like_id;
+	}
+
+	public void setLike_id(int like_id) throws LikeException {
+		if (like_id > 0) {
+			this.like_id = like_id;
+		} else {
+			throw new LikeException("You can't like this post right now, please try again later!");
+		}
 	}
 
 	public int getPost_id() {
 		return post_id;
 	}
 
-	public void setPost_id(int post_id) {
+	public void setPost_id(int post_id) throws LikeException {
 		if (post_id > 0) {
 			this.post_id = post_id;
+		} else {
+			throw new LikeException("You can't like this post right now, please try again later!");
 		}
 	}
 
@@ -24,9 +48,11 @@ public class Like {
 		return user_id;
 	}
 
-	public void setUser_id(int user_id) {
+	public void setUser_id(int user_id) throws LikeException {
 		if (user_id > 0) {
 			this.user_id = user_id;
+		} else {
+			throw new LikeException("You can't like this post right now, please try again later!");
 		}
 	}
 
